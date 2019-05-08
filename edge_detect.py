196880251e53b2cdd6cab59f62315946f9eefb56
padawong <erin.b.wong@gmail.com>
2019-05-08 15:20:34 -0700

    Accept input file name; General cleanup

--- edge_detect.py
+++ edge_detect.py
@@ -1,8 +1,19 @@
+# Input format: python edge_detect.py --input hand_drawn_test/back_pen_flash.jpg
+# If getting an imread error, first double-check correctness of file path 
+
 import cv2
 import numpy as np
+import argparse
+import os # Library will be used to take input and split the path
 
-img = cv2.cv2.imread('test.jpg', 1)
 from matplotlib import pyplot as plt
+
+parser = argparse.ArgumentParser(description='Edge detection')
+parser.add_argument('--input', help='input file')
+args = parser.parse_args()
+print(args.input)
+
+img = cv2.cv2.imread(args.input, 1)
     
 # Take each frame
 #_, frame = .read()
@@ -19,10 +30,15 @@ laplacian = cv2.Laplacian(img,cv2.CV_64F)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
 
-cv2.imwrite('test_Original.jpg',img)
-cv2.imwrite('test_Mask.jpg',mask)
-cv2.imwrite('test_laplacian.jpg',laplacian)
-cv2.imwrite('test_sobelx.jpg',sobelx)
-cv2.imwrite('test_sobely.jpg',sobely)
+# os.path automatically searches for the appropriate path marker, in this case '/'
+head, tail = os.path.split(args.input)
+print(head)
+print(tail)
+
+cv2.imwrite(os.path.join(head,'Original_' + tail), img)
+cv2.imwrite(os.path.join(head,'Mask_' + tail), mask)
+cv2.imwrite(os.path.join(head,'Laplacian_' + tail), laplacian)
+cv2.imwrite(os.path.join(head,'Sobelx_' + tail), sobelx)
+cv2.imwrite(os.path.join(head,'Sobely_' + tail), sobely)
 
 cv2.destroyAllWindows()
